89bad660d4254ed1b2463b69885742838608e350
anxhelino <anxhelinoismailanji@gmail.com>
2023-05-27 15:56:46 +0200

    charts

--- src/components/charts/Accidents.jsx
+++ src/components/charts/Accidents.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import {
   BarChart,
   Bar,
-  Cell,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer,
 } from 'recharts';
 
 const data = [
