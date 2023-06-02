ed5f0b6ce2ac36809150a55c3bfdf157169da55d
anxhelino <anxhelinoismailanji@gmail.com>
2023-06-02 15:00:47 +0200

    minor fix

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,13 +4,14 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 const Map = ({ location }) => {
   const lat = location?.lat || 59.440765;
   const lng = location?.lng || 24.755728;
+  const locationName = location?.location;
   console.log(lat);
   return (
     <div className='map'>
-      <div>{lat ? '' : <h3>Nearest Site</h3>}</div>
+      <div>{location ? '' : <h3>Nearest Site</h3>}</div>
       <div className='locationContainer'>
         <div className='locationDescription'>
-          <p>{lat ? location?.location : 'Estonia Harjumaa Tallinn Sadama'}</p>
+          <p>{location ? locationName : 'Estonia Harjumaa Tallinn Sadama'}</p>
         </div>
         <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
           <TileLayer
