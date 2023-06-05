79955e9e5829b2ea8ce4068b244949a2b0817a4c
Kubra Guran <kubraguran00@gmail.com>
2023-06-06 00:45:31 +0200

    v

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MobileNavigation from './components/MobileNavigation';
 import Navigation from './components/Navigation';
@@ -8,43 +7,29 @@ import Signup from './pages/Signup';
 import LoginPage from './pages/LoginPage';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   return (
     <div className='mainContainer'>
-    <Router>
-      {isAuthenticated && <Header />}
-      <div className='desktopContainer'>
-        {isAuthenticated && (
-          <>
-            <Navigation />
-            <MobileNavigation />
-          </>
-        )}
-        <Routes>
-          {isAuthenticated ? (
-            <>
-              <Route path='/' element={<Dashboard />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/audits' element={<Audits />} />
-              <Route path='/issues' element={<Issues />} />
-              <Route path='/policies' element={<Policies />} />
-              <Route path='/report' element={<Report />} />
-              <Route path='/sites' element={<Sites />} />
-              <Route path='/training' element={<Training />} />
-              <Route path='/diary' element={<Diary />} />
-            </>
-          ) : (
-            <>
-              <Route path='/' element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
-              <Route path='/signup' element={<Signup setIsAuthenticated={setIsAuthenticated} />} />
-            </>
-          )}
-        </Routes>
-      </div>
-    </Router>
-  </div>
-  
+      <Router>
+        <Header />
+        <div className='desktopContainer'>
+          <Navigation />
+          <MobileNavigation />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/audits' element={<Audits />} />
+            <Route path='/issues' element={<Issues />} />
+            <Route path='/policies' element={<Policies />} />
+            <Route path='/report' element={<Report />} />
+            <Route path='/sites' element={<Sites />} />
+            <Route path='/training' element={<Training />} />
+            <Route path='/diary' element={<Diary />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Audits.jsx
+++ src/pages/Audits.jsx
@@ -5,12 +5,76 @@ import SelectionCarousel from '../components/SelectionCarousel';
 const Audits = () => {
 
   return (
-    <div className="audits-container">
+    <div className = "mainAudits">
+      <div className="audits-container">
+        <h5>lorem ipsum dolor</h5>
       <MainCarousel />
       <SelectionCarousel />
     </div>
+    <div class="container-calendar">
+  <div class="time-slots">
+    <h2>Choose Time Slot</h2>
+    <ul class="time-slot-list">
+      <li>9:00 AM - 10:00 AM</li>
+      <li>10:00 AM - 11:00 AM</li>
+      <li>11:00 AM - 12:00 PM</li>
+      <li>12:00 PM - 1:00 PM</li>
+      <li>1:00 PM - 2:00 PM</li>
+      <li>2:00 PM - 3:00 PM</li>
+      <li>3:00 PM - 4:00 PM</li>
+    </ul>
+  </div>
+  <div class="notes">
+    <h2>Take Notes</h2>
+    <textarea placeholder="Enter your notes here"></textarea>
+    <button class="send-btn">Send</button>
+  </div>
+</div>
+
+<div class="updates-container">
+  <div class="update-box">
+    <h3>Latest Updates</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <a href="#" class="read-more">Read more</a>
+  </div>
+  <div class="news-box">
+    <h3>News</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <a href="#" class="read-more">Read more</a>
+  </div>
+  <div class="events-box">
+    <h3>Events</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <a href="#" class="read-more">Read more</a>
+  </div>
+  <div class="updates-box">
+    <h3>Product Updates</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <a href="#" class="read-more">Read more</a>
+  </div>
+</div>
+
+    </div>
   );
 };
+
+
+
+
+// return (
+//   <div className = "mainAudits">
+//     <div className="audits-container">
+//       <h5>lorem ipsum dolor</h5>
+//     <MainCarousel />
+//     <SelectionCarousel />
+//   </div>
+//    <div className='auditsSecond'>
+//    <h5>Lorem, ipsum dolor.</h5>
+//    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, libero!</p>
+//    </div>
+//   </div>
+// );
+// };
 
 export default Audits;
  
